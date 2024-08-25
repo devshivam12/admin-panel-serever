c3f316fbf57d3118cf52417cd695ccb5e6b56465
Shivam <mittalshivam440@gmail.com>
2024-08-25 23:58:19 +0530

    something is changed

--- controller/client.controller.js
+++ controller/client.controller.js
@@ -4,86 +4,100 @@ import Transactions from "../model/Transactions.js"
 import User from '../model/User.js'
 
 export const getProducts = async (req, res) => {
-    try {
-        const products = await Product.find()
-        console.log(products)
-        const productStats = await Promise.all(
-            products.map(async (product) => {
-                const stat = await ProductStat.find({
-                    productId: product._id
-                })
-                return {
-                    ...product._doc,
-                    stat
-                }
-            })
-        )
+  try {
+    const products = await Product.find()
+    const productStats = await Promise.all(
+      products.map(async (product) => {
+        const stat = await ProductStat.find({
+          productId: product._id
+        })
+        return {
+          ...product._doc,
+          stat
+        }
+      })
+    )
 
-        res.status(200).json({ success: true, data: productStats })
-    } catch (error) {
-        res.status(500).json({ success: false, message: error.message })
-    }
+    res.status(200).json({ success: true, data: productStats })
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message })
+  }
 }
 
 export const getCustomers = async (req, res) => {
-    try {
-        const { page = 1, limit = 10 } = req.query;
+  try {
+    const { page = 1, limit = 10 } = req.query;
 
-        const customers = await User.find({ role: "user" })
-            .select("-password")
-            .skip(page * limit)
-            .limit(limit)
+    const customers = await User.find({ role: "user" })
+      .select("-password")
+      .skip(page * limit)
+      .limit(limit)
 
-        const totalCount = await User.countDocuments({ role: "user" })
+    const totalCount = await User.countDocuments({ role: "user" })
 
-        res.status(200).json({
-            success: true,
-            data: customers,
-            totalPages: Math.ceil(totalCount / limit),
-            totalCount
-        })
-    } catch (error) {
-        res.status(500).json({ success: false, message: error.message })
-    }
+    res.status(200).json({
+      success: true,
+      data: customers,
+      totalPages: Math.ceil(totalCount / limit),
+      totalCount
+    })
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message })
+  }
 }
 
 export const getTransactions = async (req, res) => {
-    try {
-        const { page = 1, pageSize = 20, sort = null, search = "" } = req.query
+  try {
+    // sort should look like this: { "field": "userId", "sort": "desc"}
+    const { page = 1, pageSize = 20, sort = null, search = "" } = req.query;
 
-        const generalSort = () => {
-            const sortParsed = JSON.parse(sort)
-            const sortFormatted = {
-                [sortParsed.field]: (sortParsed.sort = "asc" ? 1 : -1),
-            };
+    // formatted sort should look like { userId: -1 }
+    const generateSort = () => {
+      const sortParsed = JSON.parse(sort);
+      const sortFormatted = {
+        [sortParsed.field]: (sortParsed.sort = "asc" ? 1 : -1),
+      };
 
-            return sortFormatted
-        }
+      return sortFormatted;
+    };
+    const sortFormatted = Boolean(sort) ? generateSort() : {};
 
-        const sortFormatted = Boolean(sort) ? generalSort() : {}
-
-        const transactions = await Transactions.find({
-            $or: [
-                { cost: { $regex: new RegExp(search, "i") } },
-                { userId: { $regex: new RegExp(search, "i") } }
-            ],
-        })
-            .sort(sortFormatted)
-            .skip(page * pageSize)
-            .limit(pageSize);
-
-        const total = await Transactions.countDocuments({
-            name: { $regex: search, $options: "i" }
-        });
-
-        res.status(200).json({
-            success: true,
-            transactions,
-            total
-        })
-
-    } catch (error) {
-        console.log(error)
-        res.status(500).json({ success: false, message: error.message })
+    const searchCriteria = {
+      $or: [
+        { cost: { $regex: new RegExp(search, "i") } },
+        { userId: { $regex: new RegExp(search, "i") } },
+        { products : {
+          $elemMatch : {
+            name :{$regex: new RegExp(search, "i")}
+          }
+        }}
+      ],
     }
-}
+
+    const transactions = await Transactions.find(searchCriteria)
+      .sort(sortFormatted)
+      .skip((page - 1) * pageSize)
+      .limit(pageSize);
+
+    const total = await Transactions.countDocuments(searchCriteria);
+
+    const transactionWithProducts = await Promise.all(
+      transactions.map(async (transaction) => {
+        const products = await Promise.all(
+          transaction.products.map(async (productId) => {
+            const product = await Product.findById(productId);
+            return product;
+          })
+        );
+        return { ...transaction._doc, products }
+      })
+    )
+
+    res.status(200).json({
+      transactionWithProducts,
+      total,
+    });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
