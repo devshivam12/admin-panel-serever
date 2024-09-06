d8e3b39300113cbb6124093f804dea3fa7f14df1
Shivam <mittalshivam440@gmail.com>
2024-09-06 20:01:37 +0530

    index to app

---
+++ app.js
@@ -0,0 +1,65 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import clientRoute from './routes/client.route.js'
+import managementRoute from './routes/management.route.js'
+import salesRoute from './routes/sales.route.js'
+import generalRoute from './routes/general.route.js'
+import OverallStat from './model/OverviewStats.js'
+import User from './model/User.js'
+import { dataAffiliateStat, dataOverallStat, dataProduct, dataProductStat, dataTransaction, dataUser } from './data/index.js'
+import Product from './model/Product.js'
+import ProductStat from './model/ProductStats.js'
+import Transactions from './model/Transactions.js'
+import AffiliateStat from './model/AffiliateStat.js'
+
+
+
+dotenv.config()
+
+const app = express()
+const MONGO_URL = process.env.MONGO_URL
+const corsOrigin = {
+    origin: process.env.FRONTEND_URL
+}
+
+// middleware
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cors(corsOrigin))
+
+// connections
+
+const port = process.env.PORT || 5001
+
+// Routes 
+
+app.use('/client', clientRoute)
+app.use('/general', generalRoute)
+app.use('/management', managementRoute)
+app.use('/sales', salesRoute)
+
+mongoose.connect(MONGO_URL)
+    .then(() => {
+        console.log("Database is connected")
+
+        app.listen(port, () => {
+            console.log(`Server is running at the ${port}`)
+
+            // User.insertMany(dataUser)
+
+            // Product.insertMany(dataProduct)
+
+            // ProductStat.insertMany(dataProductStat)
+
+            // Transactions.insertMany(dataTransaction)
+
+            // OverallStat.insertMany(dataOverallStat)
+
+            // AffiliateStat.insertMany(dataAffiliateStat)
+        })
+    }).catch((error) => {
+        console.log(error)
+    })
